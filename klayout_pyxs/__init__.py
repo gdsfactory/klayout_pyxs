5941378272302aeefb3c035fb4d2bda3358abb33
dpustakhod <d.pustakhod@tue.nl>
2019-01-11 00:02:51 +0100

    Extend pya/klayout imports

--- klayout_pyxs/__init__.py
+++ klayout_pyxs/__init__.py
@@ -6,8 +6,14 @@ Copyright 2017-2019 Dima Pustakhod
 """
 try:
     import klayout as klayout
+
+    from klayout.db import Box
+    from klayout.db import Edge
+    from klayout.db import EdgeProcessor as EP_
+    from klayout.db import LayerInfo
+    from klayout.db import Point
     from klayout.db import Polygon
-    from klayout.dbcore import LayerInfo
+    from klayout.db import Trans
 
 
     HAS_KLAYOUT = True
@@ -15,9 +21,20 @@ try:
 except:
     try:
         import pya as klayout
-        from pya import Polygon
-        from pya import EdgeProcessor as pya_EP
 
+        from pya import Box
+        from pya import Edge
+        from pya import EdgeProcessor as EP_
+        from pya import LayerInfo
+        from pya import Point
+        from pya import Polygon
+        from pya import Trans
+
+        # For plugin only
+        from pya import Action
+        from pya import Application
+        from pya import FileDialog
+        from pya import MessageBox
 
         HAS_PYA = True
         print('Found pya module')
