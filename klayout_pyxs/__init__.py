58d3dab709ede2bd81acdc3119f627cdee350f86
dpustakhod <d.pustakhod@tue.nl>
2019-01-10 23:08:19 +0100

    Minor changes

--- klayout_pyxs/__init__.py
+++ klayout_pyxs/__init__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-""" pyxs.__init__.py
+# coding: utf-8
+"""klayout_pyxs.__init__.py
 
-(C) 2017 Dima Pustakhod and contributors
-
+Copyright 2017-2019 Dima Pustakhod
 
 """
-
 from pya import Polygon
+
 # from .misc import info
 # reload(pyxs.misc)
 # reload(pyxs.geometry_2d)
@@ -14,9 +13,14 @@ from pya import Polygon
 
 
 def _poly_repr(self):
+    """Return nice representation of the Polygon instance
+
+    This is useful when printing a list of Polygons
+    """
     return '{} pts: '.format(self.num_points()) + self.__str__()
 
-# Print Polygon coordinates when displayed as a list element
+
 Polygon.__repr__ = _poly_repr
+
 
 # info('pyxs.__init__.py loaded')
