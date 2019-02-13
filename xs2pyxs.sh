4f9ccaf0c4cab7ff035d81e3d5f8507963d7830e
dpustakhod <d.pustakhod@tue.nl>
2019-02-13 15:25:51 +0100

    Add scripts for XS to PYXS conversion

---
+++ xs2pyxs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# This script contains replacement patterns to convert XS to PYXS
+# It implements some of the required changes, however more changes 
+# might be needed.
+
+if [ "$1" == "" ]; then
+  all_xs=( *.xs )  # will test all .pyxs files in the folder
+  xs_files=${all_xs[@]}
+else
+  xs_files=$*  # will test only specified file(s)
+fi
+
+for xs_file in $xs_files; do
+    pyxs_file=`echo $xs_file | sed 's/\.xs$/\.pyxs/'`
+    sed -r --file=xs2pyxs_patterns.txt < $xs_file > $pyxs_file
+done
