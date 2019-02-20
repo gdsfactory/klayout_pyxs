83831a8e94631b9ff77f239faf478c62384bf32e
dpustakhod <d.pustakhod@tue.nl>
2019-02-20 20:18:02 +0100

    Remove markdown from conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,10 +53,8 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
+source_suffix = ['.rst']
+
 # The master toctree document.
 master_doc = 'index'
 
@@ -83,6 +80,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
