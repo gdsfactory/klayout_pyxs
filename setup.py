4f7d31884faa284a014ca55fb051c7a28dc9c02f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-07-09 15:18:53 +0200

    remove requirements from setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
 
 
-def get_install_requires():
-    with open("requirements.txt") as f:
-        return [line.strip() for line in f.readlines() if not line.startswith("-")]
+# def get_install_requires():
+#     with open("requirements.txt") as f:
+#         return [line.strip() for line in f.readlines() if not line.startswith("-")]
 
 
 setup(
@@ -21,7 +21,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
-    install_requires=get_install_requires(),
+    # install_requires=get_install_requires(),
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python",
